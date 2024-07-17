c4f11813a1294259bc1212ba497b0a9dd3e8fa85
Carlos <116290107+CarloSol-90@users.noreply.github.com>
2024-07-17 13:41:47 +0200

    Modulo-5 refactor

--- Modulo-5/src/main.ts
+++ Modulo-5/src/main.ts
@@ -9,9 +9,6 @@ function muestraPuntuacion() {
   }
 }
 
-// Mostramos la puntuación
-muestraPuntuacion();
-
 // Invocar la función cuando el DOM esté completamente cargado
 document.addEventListener("DOMContentLoaded", () => {
   muestraPuntuacion();
@@ -82,14 +79,29 @@ const pintarUrlCarta = (urlCarta: string) => {
   }
 };
 
+const damePuntuacionCarta = (carta: number) => {
+  return carta > 7 ? 0.5 : carta;
+};
+
+const sumaPuntuacion = (puntos: number) => {
+  return puntos + puntuacion;
+};
+
+const actualizarPuntuacion = (nuevosPuntos: number) => {
+  puntuacion = nuevosPuntos;
+};
+
 //funcion principal
 const dameCarta = () => {
   const numeroAleatorio = obtenerNumeroAleatorio();
   const carta = obteneNumeroCarta(numeroAleatorio);
   const urlCarta = obtenerUrlCarta(carta);
   pintarUrlCarta(urlCarta);
-  actualizarPuntuacion(carta);
-  verificarGameOver();
+  const puntos = damePuntuacionCarta(carta);
+  const puntosSumados = sumaPuntuacion(puntos);
+  actualizarPuntuacion(puntosSumados);
+  muestraPuntuacion();
+  verificarPartida();
 };
 
 const botonDameCarta = document.getElementById("dameCarta");
@@ -109,34 +121,39 @@ const sumarPuntuacion = (numeroAleatorio: number): number => {
   return numeroAleatorio;
 };
 
-//Suma de puntuación
-// En el juego, los valores 10, 11 y 12 suman 0.5 puntos
-const actualizarPuntuacion = (carta: number): void => {
-  const valorCarta = carta > 7 ? 0.5 : carta;
-  puntuacion += valorCarta;
-  muestraPuntuacion();
-};
-
 //Verificar si la puntuación supera 7.5
-const verificarGameOver = (): void => {
+const verificarPartida = (): void => {
   if (puntuacion > 7.5) {
-    mostrarMensaje("Game Over 💀​​");
-    deshabilitarBotonCarta();
+    mostrarMensaje("Game Over 💀");
+    deshabilitarBotonCarta(true);
+  }
+
+  if (puntuacion === 7.5) {
+    mostrarMensaje("¡He ganado la partida! 🥳");
+    deshabilitarBotonCarta(true);
   }
 };
 
 //Desabilita el boton si superera 7.5
-const deshabilitarBotonCarta = (): void => {
-  const boton = document.getElementById("dameCarta") as HTMLButtonElement;
-  if (boton) {
-    boton.disabled = true;
+const deshabilitarBotonCarta = (estaHabilitado: boolean): void => {
+  const botonDameCarta = document.getElementById("dameCarta");
+  if (
+    botonDameCarta !== null &&
+    botonDameCarta !== undefined &&
+    botonDameCarta instanceof HTMLButtonElement
+  ) {
+    botonDameCarta.disabled = estaHabilitado;
   }
 };
 
 //Mostrar mensaje de "GAME OVER" por pantalla
 const mostrarMensaje = (mensaje: string): void => {
   const mensajeDiv = document.getElementById("mensaje");
-  if (mensajeDiv) {
+  if (
+    mensajeDiv !== null &&
+    mensajeDiv !== undefined &&
+    mensajeDiv instanceof HTMLDivElement
+  ) {
     mensajeDiv.textContent = mensaje;
   }
 };
@@ -144,101 +161,54 @@ const mostrarMensaje = (mensaje: string): void => {
 //Mostrar Mensaje sobre plantarme y desabilitar boton "Dame carta"
 //Funcion secundaria
 const manejarClicPlantarme = (): void => {
-  deshabilitarBotonCarta();
+  deshabilitarBotonCarta(true);
   mostrarMensajeFinal();
+};
+
+const dameMensajeFinal = () => {
+  if (puntuacion <= 4) {
+    return "Has sido muy conservador 🙄";
+  } else if (puntuacion <= 5) {
+    return "Te ha entrado el canguelo eh? 😆";
+  } else if (puntuacion > 5 && puntuacion < 7.5) {
+    return "Casi casi... 😏";
+  } else if (puntuacion === 7.5) {
+    return "¡Lo has clavado! ¡Enhorabuena! 🥳";
+  }
+
+  return "";
 };
 
 //Funcion Mensaje final
 const mostrarMensajeFinal = (): void => {
-  let mensaje = "";
-  if (puntuacion <= 4) {
-    mensaje = "Has sido muy conservador 🙄​";
-  } else if (puntuacion <= 5) {
-    mensaje = "Te ha entrado el canguelo eh? 😆​";
-  } else if (puntuacion > 5 && puntuacion < 7.5) {
-    mensaje = "Casi casi... 😏​";
-  } else if (puntuacion === 7.5) {
-    mensaje = "¡Lo has clavado! ¡Enhorabuena! 🥳​";
-  }
+  let mensaje = dameMensajeFinal();
   mostrarMensaje(mensaje);
 };
 
 // Funcionalidad boton Plantarme
 const botonPlantarme = document.getElementById("botonPlantarme");
-if (botonPlantarme) {
+if (
+  botonPlantarme !== null &&
+  botonPlantarme !== undefined &&
+  botonPlantarme instanceof HTMLButtonElement
+) {
   botonPlantarme.addEventListener("click", manejarClicPlantarme);
 }
 
-/*
-1. Mostrar puntuación ✔️​
+const nuevaPartida = () => {
+  actualizarPuntuacion(0);
+  muestraPuntuacion();
+  pintarUrlCarta("src/img/cartaBack.jpg");
+  deshabilitarBotonCarta(false);
+  mostrarMensaje("");
+};
 
-    Arranca por crear una variable que almacena la puntuación que lleve el usuario:
+const botonNuevaPartida = document.getElementById("nuevaPartida");
 
-    Crea una variable para almacenar la puntuación, inicialmente será 0. ✔️​
-    Crea un div en el HTML en el que podamos mostrar la puntuación. ✔️​
-    Crea una función que se llame muestraPuntuacion que muestre la puntuación actual en el div. ✔️​
-    Invoca a la función en cuanto este disponible el DOM. ✔️
-
-    Más adelante invocaremos muestraPuntuación cada vez que el usuario pida carta nueva.
-
-2. Pedir carta ✔️​
-
-    Implementa la funcionalidad de pedir carta, ¿En qué consiste?
-
-    Hay que generar una función que nos devuelva una carta aleatoria, la podemos llamar dameCarta.✔️​
-    Para ello exponemos un botón en el HTML que al pulsarlo llame a la función dameCarta.✔️​
-    Para probar este caso, de momento muestra la carta elegida por consola.✔️​
-    Pistas:
-    Las cartas tienen los siguientes valores: 1,2,3,4,5,6,7,10,11,12
-    Hasta ahora math.Random lo hemos usado para obtener números aleatores de un rango continuo (por ejemplo de 0 a 100), en este caso nos queremos saltar el 8 y el 9, SPOILER ALERT (piensa en una solución antes de leer la siguiente pista :))... ¿Cómo podemos hacerlo?
-    Puedes plantear generar un número aleatorio entre 1 y 10, si el número es mayor que 7, le sumas 2 y ya tienes los valores que necesitabas.✔️​
-
-3. Mostrar carta
-
-    Crea una función que se llame muestraCarta que muestre la carta que le pasemos por parámetro, la firma podría ser tal que así:
-    const mostrarCarta = (carta: number) : void;✔️​
-    Pistas
-    Añade un img en el HTML en el que podamos mostrar la carta.✔️​
-
-    Ese img va a tener un src que va a ser la url de la imagen de la carta, de momento, utiliza la imagen de carta boca abajo: https://github.com/Lemoncode/fotos-ejemplos/blob/main/cartas/back.jpg ✔️​
-
-    Crea una función mostrar carta, para mapear valor a imagen de carta puedes utilizar un switch para hacer la conversión, recuerda que más arriba tienes los enlaces a las imágenes de las cartas.✔️​
-
-    Cuando el usuario pulse en el bóton Pide Carta llama a pideCarta y con el resultado llama a mostrarCarta.✔️​
-
-3. Sumar puntuación
-
-    Una vez que le hemos dado la carta al usuario, tenemos que sumar la puntuación de la carta a la puntuación total.✔️​
-
-    Pistas
-    Tenemos un div donde mostramos la puntuación y tenemos una variable donde la almacenamos.
-    Suma el nuevo valor y llama a la función que creamos previamente para mostrar la información.✔️​
-
-4. Game over
-
-    Si el usuario se pasa de 7,5 puntos, el juego termina y se muestra un mensaje de Game Over, además el usuario no puede seguir pidiendo cartas. ✔️​
-
-5. Me planto
-
-    Añadir un botón para que el usuario pueda plantarse, si el usuario se planta, el juego termina, el usuario no puede pedir más cartas y: ✔️​
-
-    Si su puntuación es menor que 4, mostrar un mensaje que diga "Has sido muy conservador".✔️​
-
-    Si la puntuación ha sido 5, mostrar un mensaje que diga "Te ha entrado el canguelo eh?".✔️​
-
-    Si la puntuación ha sido 6 o 7, mostrar un mensaje que diga... "Casi casi...".✔️​
-
-    Si la puntuación es 7.5, mostrar un mensaje que diga "¡ Lo has clavado! ¡Enhorabuena!"✔️​
-
-6. Nueva partida
-
-    Una vez que el usuario ha terminado la partida (sea porque se ha plantado o porque ha perdido), se le muestra un botón para que pueda empezar una nueva partida.
-
-7. Estila la aplicación
-
-    Utilizando CSS, estila la aplicación (margenes, espacios, colores, etc...) para que tenga el mejor aspecto posible.
-*/
-
-/*Crear una funcion como obtenerNumeroCarta pero para darles valor a cada carta
-
-*/
+if (
+  botonNuevaPartida !== null &&
+  botonNuevaPartida !== undefined &&
+  botonNuevaPartida instanceof HTMLButtonElement
+) {
+  botonNuevaPartida.addEventListener("click", nuevaPartida);
+}
