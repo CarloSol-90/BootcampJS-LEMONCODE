da1c74f17d95a417cce2f8b826d963307cd11f8d
Carlos <116290107+CarloSol-90@users.noreply.github.com>
2024-08-06 13:03:07 +0200

    Modulo-6

--- Modulo-6/src/motor.ts
+++ Modulo-6/src/motor.ts
@@ -1,84 +1,21 @@
-import {
-  puntuacion,
-  dameMensajeFinal,
-  damePuntuacionCarta,
-  sumaPuntuacion,
-  actualizarPuntuacion,
-} from "./model";
-import { obtenerUrlCarta, pintarUrlCarta, muestraPuntuacion } from "./ui";
+import { partida } from "./model";
 
-//Devolver numero aleatorio
-export const obtenerNumeroAleatorio = (): number => {
-  return Math.floor(Math.random() * 10) + 1;
+export const damePuntuacionCarta = (carta: number) => {
+  return carta > 7 ? 0.5 : carta;
+};
+
+export const actualizarPuntuacion = (nuevosPuntos: number) => {
+  partida.puntuacion = nuevosPuntos;
+};
+
+export const sumaPuntuacion = (puntos: number) => {
+  return puntos + partida.puntuacion;
 };
 
 //Función para saltarse los numeros 8 y 9
-export const obteneNumeroCarta = (numeroAleatorio: number): number => {
+export const sumarPuntuacion = (numeroAleatorio: number): number => {
   if (numeroAleatorio > 7) {
     return numeroAleatorio + 2;
   }
   return numeroAleatorio;
-};
-
-//Verificar si la puntuación supera 7.5
-export const verificarPartida = (): void => {
-  if (puntuacion > 7.5) {
-    mostrarMensaje("Game Over 💀");
-    deshabilitarBotonCarta(true);
-  }
-
-  if (puntuacion === 7.5) {
-    mostrarMensaje("¡He ganado la partida! 🥳");
-    deshabilitarBotonCarta(true);
-  }
-};
-
-//Mostrar mensaje de "GAME OVER" por pantalla
-export const mostrarMensaje = (mensaje: string): void => {
-  const mensajeDiv = document.getElementById("mensaje");
-  if (
-    mensajeDiv !== null &&
-    mensajeDiv !== undefined &&
-    mensajeDiv instanceof HTMLDivElement
-  ) {
-    mensajeDiv.textContent = mensaje;
-  }
-};
-
-//Desabilita el boton si superera 7.5
-export const deshabilitarBotonCarta = (estaHabilitado: boolean): void => {
-  const botonDameCarta = document.getElementById("dameCarta");
-  if (
-    botonDameCarta !== null &&
-    botonDameCarta !== undefined &&
-    botonDameCarta instanceof HTMLButtonElement
-  ) {
-    botonDameCarta.disabled = estaHabilitado;
-  }
-};
-
-//Mostrar Mensaje sobre plantarme y desabilitar boton "Dame carta"
-//Funcion secundaria
-export const manejarClicPlantarme = (): void => {
-  deshabilitarBotonCarta(true);
-  mostrarMensajeFinal();
-};
-
-//Funcion Mensaje final
-export const mostrarMensajeFinal = (): void => {
-  let mensaje = dameMensajeFinal();
-  mostrarMensaje(mensaje);
-};
-
-//funcion principal
-export const dameCarta = () => {
-  const numeroAleatorio = obtenerNumeroAleatorio();
-  const carta = obteneNumeroCarta(numeroAleatorio);
-  const urlCarta = obtenerUrlCarta(carta);
-  pintarUrlCarta(urlCarta);
-  const puntos = damePuntuacionCarta(carta);
-  const puntosSumados = sumaPuntuacion(puntos);
-  actualizarPuntuacion(puntosSumados);
-  muestraPuntuacion();
-  verificarPartida();
 };
--- Modulo-6/src/ui.ts
+++ Modulo-6/src/ui.ts
@@ -1,16 +1,15 @@
-import { puntuacion, actualizarPuntuacion } from "./model";
+import { partida } from "./model";
 import {
-  dameCarta,
-  manejarClicPlantarme,
-  deshabilitarBotonCarta,
-  mostrarMensaje,
+  damePuntuacionCarta,
+  sumaPuntuacion,
+  actualizarPuntuacion,
 } from "./motor";
 
 // Función para mostrar la puntuación actual en el div
 export function muestraPuntuacion() {
   const puntuacionDiv = document.getElementById("puntuacion");
   if (puntuacionDiv) {
-    puntuacionDiv.innerText = "Puntuación actual: " + puntuacion;
+    puntuacionDiv.innerText = "Puntuación actual: " + partida.puntuacion;
   }
 }
 
@@ -66,6 +65,19 @@ export const pintarUrlCarta = (urlCarta: string) => {
   }
 };
 
+//funcion principal
+export const dameCarta = () => {
+  const numeroAleatorio = obtenerNumeroAleatorio();
+  const carta = obteneNumeroCarta(numeroAleatorio);
+  const urlCarta = obtenerUrlCarta(carta);
+  pintarUrlCarta(urlCarta);
+  const puntos = damePuntuacionCarta(carta);
+  const puntosSumados = sumaPuntuacion(puntos);
+  actualizarPuntuacion(puntosSumados);
+  muestraPuntuacion();
+  verificarPartida();
+};
+
 export const botonDameCarta = document.getElementById("dameCarta");
 if (
   botonDameCarta !== null &&
@@ -74,6 +86,13 @@ if (
 ) {
   botonDameCarta.addEventListener("click", dameCarta);
 }
+
+//Mostrar Mensaje sobre plantarme y desabilitar boton "Dame carta"
+//Funcion secundaria
+export const manejarClicPlantarme = (): void => {
+  deshabilitarBotonCarta(true);
+  mostrarMensajeFinal();
+};
 
 // Funcionalidad boton Plantarme
 const botonPlantarme = document.getElementById("botonPlantarme");
@@ -102,3 +121,73 @@ if (
 ) {
   botonNuevaPartida.addEventListener("click", nuevaPartida);
 }
+
+//Devolver numero aleatorio
+export const obtenerNumeroAleatorio = (): number => {
+  return Math.floor(Math.random() * 10) + 1;
+};
+
+//Función para saltarse los numeros 8 y 9
+export const obteneNumeroCarta = (numeroAleatorio: number): number => {
+  if (numeroAleatorio > 7) {
+    return numeroAleatorio + 2;
+  }
+  return numeroAleatorio;
+};
+
+//Verificar si la puntuación supera 7.5
+export const verificarPartida = (): void => {
+  if (partida.puntuacion > 7.5) {
+    mostrarMensaje("Game Over 💀");
+    deshabilitarBotonCarta(true);
+  }
+
+  if (partida.puntuacion === 7.5) {
+    mostrarMensaje("¡He ganado la partida! 🥳");
+    deshabilitarBotonCarta(true);
+  }
+};
+
+//Mostrar mensaje de "GAME OVER" por pantalla
+export const mostrarMensaje = (mensaje: string): void => {
+  const mensajeDiv = document.getElementById("mensaje");
+  if (
+    mensajeDiv !== null &&
+    mensajeDiv !== undefined &&
+    mensajeDiv instanceof HTMLDivElement
+  ) {
+    mensajeDiv.textContent = mensaje;
+  }
+};
+
+//Desabilita el boton si superera 7.5
+export const deshabilitarBotonCarta = (estaHabilitado: boolean): void => {
+  const botonDameCarta = document.getElementById("dameCarta");
+  if (
+    botonDameCarta !== null &&
+    botonDameCarta !== undefined &&
+    botonDameCarta instanceof HTMLButtonElement
+  ) {
+    botonDameCarta.disabled = estaHabilitado;
+  }
+};
+
+//Funcion Mensaje final
+export const mostrarMensajeFinal = (): void => {
+  let mensaje = dameMensajeFinal();
+  mostrarMensaje(mensaje);
+};
+
+export const dameMensajeFinal = () => {
+  if (partida.puntuacion <= 4) {
+    return "Has sido muy conservador 🙄";
+  } else if (partida.puntuacion <= 5) {
+    return "Te ha entrado el canguelo eh? 😆";
+  } else if (partida.puntuacion > 5 && partida.puntuacion < 7.5) {
+    return "Casi casi... 😏";
+  } else if (partida.puntuacion === 7.5) {
+    return "¡Lo has clavado! ¡Enhorabuena! 🥳";
+  }
+
+  return "";
+};
