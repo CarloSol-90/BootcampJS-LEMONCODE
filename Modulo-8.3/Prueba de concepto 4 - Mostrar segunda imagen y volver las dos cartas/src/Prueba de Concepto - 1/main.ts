41dd54e328ec0379dea52b8c82e9b4a579e8b84c
Carlos <116290107+CarloSol-90@users.noreply.github.com>
2024-08-28 13:46:44 +0200

    Prueba de concepto 4 - actualizada

--- Modulo-8.3/Prueba de concepto 4 - Mostrar segunda imagen y volver las dos cartas/src/Prueba de Concepto - 1/main.ts
+++ Modulo-8.3/Prueba de concepto 4 - Mostrar segunda imagen y volver las dos cartas/src/Prueba de Concepto - 1/main.ts
@@ -1,12 +1,24 @@
-const carta1 = document.getElementById("carta1");
+/*"https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png"*/
+
+const carta1 = document.querySelector(`div[data-indice-id="${0}"]`);
+const carta2 = document.querySelector(`div[data-indice-id="${1}"]`);
 
 if (carta1) {
   carta1.addEventListener("click", () => {
-    const imagen1 = document.getElementById("imagen1");
+    const imagen1 = document.querySelector(`img[data-indice-id="${0}"]`);
     if (imagen1 && imagen1 instanceof HTMLImageElement) {
-      console.log("Holaa");
       imagen1.src =
         "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png";
+    }
+  });
+}
+
+if (carta2) {
+  carta2.addEventListener("click", () => {
+    const imagen1 = document.querySelector(`img[data-indice-id="${1}"]`);
+    if (imagen1 && imagen1 instanceof HTMLImageElement) {
+      imagen1.src =
+        "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/2.png";
     }
   });
 }
