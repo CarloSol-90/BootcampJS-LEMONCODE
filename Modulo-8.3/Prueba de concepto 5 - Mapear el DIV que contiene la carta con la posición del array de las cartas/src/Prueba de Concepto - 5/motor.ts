574d4bed86931a7d703c849aa153228658691237
Carlos <116290107+CarloSol-90@users.noreply.github.com>
2024-09-05 10:02:45 +0200

    Prueba de concepto 5 - Mapear el DIV que contiene la carta con la posición del array de las cartas

--- Modulo-8.3/Prueba de concepto 5 - Mapear el DIV que contiene la carta con la posición del array de las cartas/src/Prueba de Concepto - 5/motor.ts
+++ Modulo-8.3/Prueba de concepto 5 - Mapear el DIV que contiene la carta con la posición del array de las cartas/src/Prueba de Concepto - 5/motor.ts
@@ -1,4 +1,4 @@
-import { Carta } from "./modelo";
+import { Carta, Tablero } from "./modelo";
 
 /*
 En el motor nos va a hacer falta un método para barajar cartas
@@ -8,10 +8,94 @@ export const barajarCartas = (cartas: Carta[]): Carta[] => {
 
   for (let i = cartasBarajadas.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1)); // Escoge un índice al azar entre 0 e i
-    [cartasBarajadas[i], cartasBarajadas[j]] = [
+    [{ ...cartasBarajadas[i] }, { ...cartasBarajadas[j] }] = [
       cartasBarajadas[j],
       cartasBarajadas[i],
     ]; // Intercambia las cartas
   }
   return cartasBarajadas;
+};
+
+/*
+  Una carta se puede voltear si no está encontrada y no está ya volteada, o no hay dos cartas ya volteadas
+*/
+export const sePuedeVoltearLaCarta = (
+  tablero: Tablero,
+  indice: number
+): boolean => {
+  return (
+    !tablero.cartas[indice].encontrada && !tablero.cartas[indice].estaVuelta
+  );
+  //si son false, que devuelvan true
+};
+
+export const voltearLaCarta = (tablero: Tablero, indice: number): void => {
+  tablero.cartas[indice].estaVuelta = true;
+  if (tablero.estadoPartida === "CeroCartasLevantadas") {
+    tablero.indiceCartaVolteadaA = indice;
+    tablero.estadoPartida = "UnaCartaLevantada";
+  } else if (tablero.estadoPartida === "UnaCartaLevantada") {
+    tablero.indiceCartaVolteadaB = indice;
+    tablero.estadoPartida = "DosCartasLevantadas";
+  }
+};
+/*
+/*
+  Dos cartas son pareja si en el array de tablero de cada una tienen el mismo id
+*/
+export const sonPareja = (
+  indiceA: number,
+  indiceB: number,
+  tablero: Tablero
+): boolean => {
+  return tablero.cartas[indiceA].idFoto === tablero.cartas[indiceB].idFoto;
+};
+
+/*
+  Aquí asumimos ya que son pareja, lo que hacemos es marcarlas como encontradas y comprobar si la partida esta completa.
+*/
+export const parejaEncontrada = (
+  tablero: Tablero,
+  indiceA: number,
+  indiceB: number
+): void => {
+  tablero.cartas[indiceA].encontrada = true;
+  tablero.cartas[indiceB].encontrada = true;
+  tablero.indiceCartaVolteadaA = undefined;
+  tablero.indiceCartaVolteadaB = undefined;
+  tablero.estadoPartida = "CeroCartasLevantadas";
+};
+
+/*
+  Aquí asumimos que no son pareja y las volvemos a poner boca abajo
+*/
+export const parejaNoEncontrada = (
+  tablero: Tablero,
+  indiceA: number,
+  indiceB: number
+): void => {
+  tablero.cartas[indiceA].encontrada = false;
+  tablero.cartas[indiceB].encontrada = false;
+  tablero.indiceCartaVolteadaA = undefined;
+  tablero.indiceCartaVolteadaB = undefined;
+  tablero.estadoPartida = "CeroCartasLevantadas";
+  tablero.cartas[indiceA].estaVuelta = false;
+  tablero.cartas[indiceB].estaVuelta = false;
+};
+
+/*
+  Esto lo podemos comprobar o bien utilizando every, o bien utilizando un contador (cartasEncontradas)
+*/
+//export const esPartidaCompleta(tablero: Tablero) : boolean => {
+//...
+//}
+
+/*
+Iniciar partida
+*/
+
+export const iniciaPartida = (tablero: Tablero): void => {
+  const cartaBarajada: Carta[] = barajarCartas(tablero.cartas);
+  tablero.cartas = [...cartaBarajada];
+  tablero.estadoPartida = "CeroCartasLevantadas";
 };
--- Modulo-8.3/Prueba de concepto 5 - Mapear el DIV que contiene la carta con la posición del array de las cartas/src/Prueba de Concepto - 5/ui.ts
+++ Modulo-8.3/Prueba de concepto 5 - Mapear el DIV que contiene la carta con la posición del array de las cartas/src/Prueba de Concepto - 5/ui.ts
@@ -1,24 +1,119 @@
 //Crear un bucle for que vaya iterandose sobre cada data indice id de cada div con el add eventlistener.
 //en cada vuelta del buclre creae un console.log con un saludo para comprobar su funcionamiento
-import { tablero } from "./modelo";
+import { Carta, tablero, Tablero } from "./modelo";
+import {
+  sePuedeVoltearLaCarta,
+  iniciaPartida,
+  voltearLaCarta,
+  sonPareja,
+  parejaEncontrada,
+  parejaNoEncontrada,
+} from "./motor";
 
-const crearTablero = () => {
-  console.log("HOLaa");
+export const crearTablero = () => {
   for (let indice = 0; indice < tablero.cartas.length; indice++) {
-    const dataIndiceId = `[data-indice-id="${indice}"]`;
-    const elementoDiv = document.querySelector(`div${dataIndiceId}`);
-    if (
-      elementoDiv !== null &&
-      elementoDiv !== undefined &&
-      elementoDiv instanceof HTMLDivElement
-    ) {
-      elementoDiv.addEventListener("click", () => {
-        console.log("Hola!!");
-      });
+    mapearDivsACartas(indice, tablero);
+  }
+};
+
+const mapearDivsACartas = (indiceCarta: number, tablero: Tablero) => {
+  const dataIndiceId = `[data-indice-id="${indiceCarta}"]`;
+  const elementoDiv = document.querySelector(`div${dataIndiceId}`);
+  if (
+    elementoDiv !== null &&
+    elementoDiv !== undefined &&
+    elementoDiv instanceof HTMLDivElement
+  ) {
+    elementoDiv.addEventListener("click", () => {
+      if (tablero.estadoPartida !== "PartidaNoIniciada") {
+        manejadorDivCarta(indiceCarta, tablero);
+      }
+    });
+  }
+};
+
+const manejadorDivCarta = (indiceCarta: number, tablero: Tablero) => {
+  if (sePuedeVoltearLaCarta(tablero, indiceCarta)) {
+    volteaCarta(indiceCarta, tablero);
+  } else {
+    //Crear un mensaje en pantalla que muestre el mensaje
+    alert("No se le puede dar la vuelta a la carta");
+  }
+};
+
+const volteaCarta = (indiceCarta: number, tablero: Tablero) => {
+  voltearLaCarta(tablero, indiceCarta);
+  mostrarImagenAnimal(indiceCarta);
+  mirarSiEsLaSegundaCarta(tablero);
+};
+
+export const agregarEventoBotonIniciarPartida = () => {
+  const botonEmpezarPartida = document.getElementById("botonIniciarPartida");
+  if (
+    botonEmpezarPartida !== null &&
+    botonEmpezarPartida !== undefined &&
+    botonEmpezarPartida instanceof HTMLButtonElement
+  ) {
+    botonEmpezarPartida.addEventListener("click", () => {
+      clickBotonEmpezarPartida();
+    });
+  }
+};
+
+const clickBotonEmpezarPartida = () => {
+  iniciaPartida(tablero);
+};
+
+const mostrarImagenAnimal = (indiceCarta: number) => {
+  const dataIndiceId = `[data-indice-id="${indiceCarta}"]`;
+  const elementoImagen = document.querySelector(`img${dataIndiceId}`);
+
+  if (
+    elementoImagen !== null &&
+    elementoImagen !== undefined &&
+    elementoImagen instanceof HTMLImageElement
+  ) {
+    elementoImagen.src = tablero.cartas[indiceCarta].imagen;
+  }
+};
+
+const mirarSiEsLaSegundaCarta = (tablero: Tablero) => {
+  const indiceA = tablero.indiceCartaVolteadaA;
+  const indiceB = tablero.indiceCartaVolteadaB;
+  if (indiceA !== undefined && indiceB !== undefined) {
+    if (sonPareja(indiceA, indiceB, tablero)) {
+      parejaEncontrada(tablero, indiceA, indiceB);
+      //crear funcion para comprobar de que hemos encontrado toddas las parejas
+    } else {
+      parejaNoEncontrada(tablero, indiceA, indiceB);
+      voltearLasCartasQueNoSonPareja(tablero.cartas);
     }
   }
+};
 
-  document.addEventListener("DOMContentLoaded", () => {
-    crearTablero();
-  });
+const voltearLasCartasQueNoSonPareja = (cartas: Carta[]) => {
+  setTimeout(() => {
+    ponerImagenBocaAbajo(cartas);
+  }, 1000);
+};
+
+const ponerImagenBocaAbajo = (cartas: Carta[]) => {
+  for (let indice = 0; indice < cartas.length; indice++) {
+    if (!cartas[indice].encontrada && !cartas[indice].estaVuelta) {
+      darleLaVueltaALaCarta(indice);
+    }
+  }
+};
+
+const darleLaVueltaALaCarta = (indiceCarta: number) => {
+  const dataIndiceId = `[data-indice-id="${indiceCarta}"]`;
+  const elementoImagen = document.querySelector(`img${dataIndiceId}`);
+
+  if (
+    elementoImagen !== null &&
+    elementoImagen !== undefined &&
+    elementoImagen instanceof HTMLImageElement
+  ) {
+    elementoImagen.src = "";
+  }
 };
