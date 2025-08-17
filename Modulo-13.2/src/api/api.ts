0ee61152bcb92023be97937046d878b38050c0dd
Carlos <116290107+CarloSol-90@users.noreply.github.com>
2025-08-17 19:44:38 +0200

    Fix Modulo-13.2: actualizar configuracion puertos y limpiar BD

--- Modulo-13.2/src/api/api.ts
+++ Modulo-13.2/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Movement, Account, CreateAccountRequest } from "./api-model";
 
-const baseURL = import.meta.env.VITE_BASE_API_URL || "http://localhost:3000";
+const baseURL = import.meta.env.VITE_BASE_API_URL || "http://localhost:3005";
 
 const apiClient = axios.create({
   baseURL,
